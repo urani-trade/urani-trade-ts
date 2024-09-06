b6733a4f70ac638c9abc0e5011d4ef600fc03c6d
Urani Helper <162904807+urani-engineering@users.noreply.github.com>
2024-09-05 22:55:12 -0700

    Update WhySolana.tsx

--- src/components/why-solana/WhySolana.tsx
+++ src/components/why-solana/WhySolana.tsx
@@ -47,7 +47,7 @@ export default function WhySolana() {
             <p className="text-1xl md:text-2xl italic">
             Onchain-agnostic orderbook conducting batch competitions among agents for the largest surplus, efficiency, and ingenuity. 
             The protocol provides various strategies to ensure fairness, accessibility, and meritocracy. 
-            Learn more <a href="https://docs.urani.trade/urani-protocol/urani-protocol-overview" target="_blank" rel="noopener noreferrer">here</b></a>.
+            Learn more <b><a href="https://docs.urani.trade/urani-protocol/urani-protocol-overview" target="_blank" rel="noopener noreferrer">here</a></b>.
               
             </p>
           </li>
@@ -64,7 +64,7 @@ export default function WhySolana() {
             <p className="text-1xl md:text-2xl italic">
               The interface and SDK facilitating the submission of order intents for consumption by the Urani Protocol, 
               supporting usage by both retail users and other protocols. 
-              Learn more <b><a href="https://docs.urani.trade/urani-swap/overview" target="_blank" rel="noopener noreferrer">here</b></a>.
+              Learn more <b><a href="https://docs.urani.trade/urani-swap/overview" target="_blank" rel="noopener noreferrer">here</a></b>.
             </p>
           </li>
           
@@ -82,7 +82,7 @@ export default function WhySolana() {
               Open-source algorithms (bots) for the Urani Protocol, allowing operators to integrate their preferred private strategies
               and start playing our MEV games. Advanced strategies and AI-centric agents are the focus of research and development in our labs. 
               Additionally, we have in-house agents running for gauging and fallback mechanisms.
-              Learn more <b><a href="https://docs.urani.trade/mev-agents/the-onboarding-process" target="_blank" rel="noopener noreferrer">here</b></a>.
+              Learn more <b><a href="https://docs.urani.trade/mev-agents/the-onboarding-process" target="_blank" rel="noopener noreferrer">here</a></b>.
 
             </p>
           </li>
@@ -100,7 +100,7 @@ export default function WhySolana() {
             <p className="text-1xl md:text-2xl italic">
               Real-time visual infrastructure for orderflow auction competition and peer-to-peer order matches, 
               designed to commoditize MEV agents and engage their fans.
-              Learn more <b><a href="https://docs.urani.trade/urani-arena/urani-arena-overview" target="_blank" rel="noopener noreferrer">here</b></a>.
+              Learn more <b><a href="https://docs.urani.trade/urani-arena/urani-arena-overview" target="_blank" rel="noopener noreferrer">here</a></b>.
        
             </p>
           </li>
