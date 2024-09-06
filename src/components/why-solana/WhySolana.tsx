472b8282f900f44d7c884010c140f68c1d4ba6a5
Urani Helper <162904807+urani-engineering@users.noreply.github.com>
2024-09-05 23:00:44 -0700

    Update WhySolana.tsx

--- src/components/why-solana/WhySolana.tsx
+++ src/components/why-solana/WhySolana.tsx
@@ -46,7 +46,7 @@ export default function WhySolana() {
             </p>
             <p className="text-1xl md:text-2xl italic">
             Onchain-agnostic orderbook conducting batch competitions among agents for the largest surplus and efficiency.
-            The protocol provides various strategies to ensure fairness, accessibility, and meritocracy. 
+            The protocol provides various strategies to ensure fairness and meritocracy. 
             <b><a href="https://docs.urani.trade/urani-protocol/urani-protocol-overview" target="_blank" rel="noopener noreferrer"> Learn more</a></b>.
               
             </p>
